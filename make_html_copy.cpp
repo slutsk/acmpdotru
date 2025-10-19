aab82871b960adb9d5d0f9ff481ea25b3b80e15c
slutsk <54749775+slutsk@users.noreply.github.com>
2025-10-19 14:55:32 +0300

    Update make_html_copy.cpp

--- make_html_copy.cpp
+++ make_html_copy.cpp
@@ -4,6 +4,8 @@
 #include<fstream>
 #include<ctime>
 #include<iomanip>
+
+
 using namespace std;
 
 struct ZADACHA{
@@ -77,7 +79,7 @@ void build_page(vector<PERSON> &vc, vector <ZADACHA> &zadacha)
      string str;
      time_t t = time(nullptr);
 	 tm *now = localtime(&t);
-		  
+
      while(getline(readFile, str))
      {
          if (str.find("lastUpdate") != std::string::npos)
@@ -111,7 +113,7 @@ void build_page(vector<PERSON> &vc, vector <ZADACHA> &zadacha)
 
                  writeFile << x.name;
                  writeFile << "<br>" << x.solved_problems;
-                 writeFile << "<br>" << x.rang; 
+                 writeFile << "<br>" << x.rang;
                  writeFile << "<br>" << x.k <<" (" << x.mx <<")"<< "</th>" << "\n";
              }
              writeFile << "</tr></thead>" << "\n";
@@ -150,6 +152,92 @@ void build_page(vector<PERSON> &vc, vector <ZADACHA> &zadacha)
      readFile.close();
 }
 
+void build_page20(vector<PERSON> &vc, vector <ZADACHA> &zadacha)
+{
+     ofstream writeFile("index2.html", ios::binary);
+     time_t t = time(nullptr);
+	 tm *now = localtime(&t);
+
+     writeFile << "<!DOCTYPE html>" <<  '\n'
+        << "<html lang=\"ru\">" << '\n'
+        << "<head>" << '\n'
+        << "<meta charset=\"UTF-8\">" << '\n'
+        << "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" << '\n'
+        << "<title>Олимпиадные задачи</title>" << "\n"
+        << "<base target=\"_blank\">" << "\n"
+        << "<link rel=\"stylesheet\" href=\"css2.css\">" << '\n'
+        << "</head>" << '\n'
+        << "<body><div class = \"main\">" << '\n' << "<table>" << '\n'
+        << "<div class = \"lastUpdate\">" << "\nLatest website update: "
+        <<setfill('0') << setw(2) << (now->tm_mday) << '.'
+        <<setfill('0') << setw(2) << (now->tm_mon+1) <<'.'
+        <<setfill('0') << setw(2) << (now->tm_year+1900) <<' '
+        <<setfill('0') << setw(2) << (now->tm_hour+3) <<':'
+        <<setfill('0') << setw(2) << (now->tm_min) << " Problems: " <<zadacha.size()
+        << "</div>"<< "\n"
+        << "<div class = \"rang_colors\"><div class = \"CL1\">500</div><div class = \"CL9\">3000</div><div class = \"CL2\">7000</div><div class = \"CL3\">13000</div><div class = \"CL4\">23000</div><div class = \"CL5\">33000</div><div class = \"CL6\">43000</div><div class = \"CL8\">63000</div></div>" << '\n'
+        << "<thead>" << '\n';
+
+
+        writeFile << "<tr>" << "<th>№ п/п</th><th>%</th><th>Задача</th>";
+        for(auto x: vc)
+             {
+                writeFile << "<th class = \"";
+                if(x.rang < 500) writeFile << "B0";
+                else if(x.rang < 3000) writeFile << "B1";
+                else if(x.rang < 7000) writeFile << "B9";
+                else if(x.rang < 13000) writeFile << "B2";
+                else if(x.rang < 23000) writeFile << "B3";
+                else if(x.rang < 33000) writeFile << "B4";
+                else if(x.rang< 43000) writeFile << "B5";
+                else if(x.rang < 53000) writeFile << "B6";
+                else if(x.rang < 63000) writeFile << "B7";
+                else if(x.rang < 100000) writeFile << "B8";
+
+                writeFile << "\">";
+
+                 writeFile << x.name;
+                 writeFile << "<br>" << x.solved_problems;
+                 writeFile << "<br>" << x.rang;
+                 writeFile << "<br>" << x.k <<" (" << x.mx <<")"<< "</th>" << "\n";
+             }
+             writeFile << "</tr></thead>" << "\n";
+
+
+             //----------------
+    int i = 1;
+    for(auto x: zadacha){
+      if(x.procent < 100){
+        writeFile << "\t\t\t<tr>";
+        writeFile << "<td>" << i++ << "</td>";
+        writeFile << "<td>" << x.procent << "%</td>";
+        writeFile << "<td><a href =\"";
+        for(auto c: x.link) writeFile << c;
+        writeFile << "\">" << x.number << "</a></td>";
+        writeFile <<"</td>";
+        int num = 0;
+        for(auto P: vc)
+        {
+            writeFile << "<td class =\"";
+            if(P.flag[x.number] and num%2) writeFile << "accepted1\"";
+            if(P.flag[x.number] and num%2==0) writeFile << "accepted2\"";
+            else writeFile << "notAccepted\"";
+            writeFile << "></td>";
+            num++;
+        }
+        writeFile << "</tr>" << "\n";
+      }
+
+    }
+             //----------------
+    writeFile << "</table></body></html>" ;
+
+
+
+
+     writeFile.close();
+
+}
 
 int main()
 {
@@ -166,7 +254,7 @@ int main()
         int mn = min((int)n, (int)x.cnt_procent.size());
         int sum = 0;
         sort(x.cnt_procent.rbegin(), x.cnt_procent.rend());
-        //std::cout <<x.name << " ";  
+        //std::cout <<x.name << " ";
         for(int i = 0; i < mn ; i++){
            sum += x.cnt_procent[i];
         }
@@ -180,15 +268,8 @@ int main()
              return a.solved_problems > b.solved_problems;
          });
 	build_page(person, zadacha);
+	build_page20(person, zadacha);
 
 
     return 0;
 }
-
-
-
-
-
-
-
-
